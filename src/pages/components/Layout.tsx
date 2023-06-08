6e118b37781ddae85f7765a75f81535625eba92c
Tadeáš Kapic <mujmeyl@gmail.com>
2023-06-08 18:12:56 +0200

    title

--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Head>
-        <title>Uhumdrum</title>
+        <title>Uhumdrum | Creative Studio based in Void</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <header className="fixed z-[999] w-[100%] ">
